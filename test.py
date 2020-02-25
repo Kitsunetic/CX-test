68f5c17629aa0ea89093693526154aafcfff028f
Kitsunetic <kitsune.guru@gmail.com>
2020-02-25 10:12:43 +0900

    update test.py

--- test.py
+++ test.py
@@ -2,15 +2,14 @@ import os
 import pickle
 import random
 
-import torch
-import torch.nn as nn
-import torch.functional as F
-import numpy as np
-import rawpy
 import imageio
 import matplotlib.pyplot as plt
-import imageio
+import numpy as np
+import rawpy
 import skimage.transform
+import torch
+import torch.nn as nn
+from PIL import Image
 from torchvision import transforms
 from tqdm import tqdm
 
@@ -18,7 +17,7 @@ from ContextualLoss import Contextual_Loss, Distance_Type
 
 black_lv = 512
 white_lv = 16383
-n_epochs = 20
+n_epochs = 15
 shift_sign = -1
 
 rgb_mean = np.array([0.14428656, 0.11273184, 0.10870461, 0.13277838])
@@ -40,8 +39,10 @@ class RAW2RGB(torch.utils.data.Dataset):
     
     self.train_path = os.path.join(dataset_path, 'train')
     self.test_path = os.path.join(dataset_path, 'test')
-    self.train_list = list(filter(lambda f: f.endswith('.ARW'), os.listdir(self.train_path)))
-    self.test_list = list(filter(lambda f: f.endswith('.JPG'), os.listdir(self.test_path)))
+    self.train_list, self.test_list = [], []
+    for train, test in zip(os.listdir(self.train_path), os.listdir(self.test_path)):
+      self.train_list.append(os.path.join(self.train_path, train))
+      self.test_list.append(os.path.join(self.test_path, test))
 
     with open(os.path.join(dataset_path, 'wb.txt'), 'r') as f:
       self.white_balance = []
@@ -55,11 +56,18 @@ class RAW2RGB(torch.utils.data.Dataset):
       train = np.array(raw.raw_image_visible, dtype=np.float32)
       train = (train-black_lv) / (white_lv-black_lv)
     
-    test = imageio.imread(self.test_list[idx])
+    #test = imageio.imread(self.test_list[idx])
+    test = Image.open(self.test_list[idx])
     
-    transform = transforms.ToTensor()
-    train = transform(train)
-    test = transform(test)
+    transform_train = transforms.ToTensor()
+    transform_test = transforms.Compose([
+      transforms.Resize((test.height//2, test.width//2)),
+      transforms.ToTensor()
+    ])
+    
+    # 이미지 스케일 up이 없기 때문에 test의 사이즈가 2배가 됨
+    train = transform_train(train)
+    test = transform_test(test)
     wb = torch.Tensor(self.white_balance[idx])
     return train, test, wb, idx
 
@@ -69,7 +77,9 @@ class RAW2RGB(torch.utils.data.Dataset):
                len(self.white_balance))
 
 def main():
-  device = torch.device('cpu')
+  os.makedirs('./result', exist_ok=True)
+  
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
   dataset = RAW2RGB('./datasets')
   dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
@@ -84,47 +94,74 @@ def main():
   
   history = {'cost': []}
   for epoch in range(1, n_epochs+1):
-    with tqdm(total=len(dataset), ncols=128, position=0, miniters=1) as t:
+    with tqdm(total=len(dataset), desc='[%04d/%04d]'%(epoch, n_epochs), ncols=128, position=0, miniters=1) as t:
       for step, (train, test, wb, idx) in enumerate(dataloader):
         # to device
         train = train.to(device)
         test = test.to(device)
-        wb = wb.to(device)
+        wb = wb.to(device) # dataloader에서 (1, 4) -> (1, 1, 4)로 변환됨
         
         # adjust wb
-        train[0, 0::2, 0::2] -= wb[0]
-        train[0, 1::2, 0::2] -= wb[1]
-        train[0, 1::2, 1::2] -= wb[2]
-        train[0, 0::2, 1::2] -= wb[3]
+        train[:, 0::2, 0::2] -= wb[0][0]
+        train[:, 1::2, 0::2] -= wb[0][1]
+        train[:, 1::2, 1::2] -= wb[0][2]
+        train[:, 0::2, 1::2] -= wb[0][3]
         
         # forward
         result = model(train)
-        cost = loss(result, test)
-        
-        # backward
-        optimizer.zero_grad()
-        cost.backward()
-        optimizer.step()
         
         # disadjust wb
         """
         4->3 채널로 변경되었으므로 몇 번 wb를 적용해야 할 지 모르겠다.
         
         Task 1.
-          wb가 RGBG이므로 wb[0, 1, 2], wb[0, 2, 3] 으로 적용해본다.
+          wb가 RGBG이므로 wb[0, 1, 2], wb[0, 2, 3], wb[0, 1+3/2, 2] 으로 적용해본다.
           
         Task 2.
           target 이미지의 평균값으로 normalize를 해준다.
         """
+        result[:, 0, :, :] += wb[0][0]
+        result[:, 1, :, :] += wb[0][1]
+        result[:, 2, :, :] += wb[0][2]
+        #result[:, 0, :, :] += wb[0][0]
+        #result[:, 1, :, :] += wb[0][2]
+        #result[:, 2, :, :] += wb[0][3]
+        #result[:, 0, :, :] += wb[0][0]
+        #result[:, 1, :, :] += (wb[0][1]+wb[0][3])/2
+        #result[:, 2, :, :] += wb[0][2]
+        
+        # crop result -> raw파일의 w/h가 16pixel씩 크므로, 1/2사이즈 된 result에서 상하좌우 4픽셀씩 잘라낸다
+        result = result[:, :, 4:-4, 4:-4]
+        
+        # calculate loss
+        #print(result.shape, test.shape)
+        cost = loss(result, test)
+        # TODO: CX, CoBi loss 적용
+        
+        # backward
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
         
         # update tqdm
         cost_val = cost.item()
         history['cost'].append(cost_val)
-        t.set_postfix_str('%.04f'%cost_val)
+        t.set_postfix_str('cost %.04f'%cost_val)
         t.update()
     
-  # show result
-  # TODO
+      # save result
+      if epoch == 1:
+        test_image = transforms.ToPILImage()(test[0])
+        test_image.save('./result/test.png')
+        test_image.close()
+      
+      if epoch % 5 == 0:
+        result_image = transforms.ToPILImage()(result[0])
+        result_image.save('./result/result_%d.png'%epoch)
+        result_image.close()
+        
+        with open('./result/history_%d.pkl'%epoch, 'wb') as f:
+          pickle.dump(history, f)
 
 if __name__ == "__main__":
   main()
